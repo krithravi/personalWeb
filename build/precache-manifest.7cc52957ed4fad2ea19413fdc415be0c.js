9d4a25cd090ac50f04a4fa39cf332cb26d1c33dc
Krithika Ravishankar <krithika.x.ravishankar@gmail.com>
2020-06-19 00:04:37 -0500

    added build

---
+++ build/precache-manifest.7cc52957ed4fad2ea19413fdc415be0c.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b72e51cc0c9d63eef9c8c79b4f00b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42dff4b52dd4b3f12493",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "a79d12511ab058c92c3f",
+    "url": "/static/css/main.8336b17d.chunk.css"
+  },
+  {
+    "revision": "42dff4b52dd4b3f12493",
+    "url": "/static/js/2.1433c8d3.chunk.js"
+  },
+  {
+    "revision": "60657446a170af885131fc1b508540c9",
+    "url": "/static/js/2.1433c8d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79d12511ab058c92c3f",
+    "url": "/static/js/main.0649b79a.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "82d1d9ed786993d9bab445b71262396a",
+    "url": "/static/media/abstract_charcoal.82d1d9ed.jpg"
+  },
+  {
+    "revision": "1f19dc9f30fae1b5fa956ff9cb1d8487",
+    "url": "/static/media/abstract_values_colored.1f19dc9f.jpg"
+  },
+  {
+    "revision": "dc79c4c4331d9d5953329e7ef6902f3a",
+    "url": "/static/media/africanbullfrog.dc79c4c4.png"
+  },
+  {
+    "revision": "f9ddf41077e27eca4e2f0d3e01fc16a3",
+    "url": "/static/media/anita.f9ddf410.jpg"
+  },
+  {
+    "revision": "77e733e3ef9383f4517a1ede4a571d0b",
+    "url": "/static/media/anthonypafrogs.77e733e3.png"
+  },
+  {
+    "revision": "ca8ad20f0a8e259e4c58462d033735b3",
+    "url": "/static/media/aquariumjelly.ca8ad20f.jpg"
+  },
+  {
+    "revision": "2e764e80e59d743ce7aef2cbb543c50e",
+    "url": "/static/media/brain.2e764e80.jpg"
+  },
+  {
+    "revision": "f8bc6310dc4c1d9cadea33e57e42f3ad",
+    "url": "/static/media/coqui.f8bc6310.jpg"
+  },
+  {
+    "revision": "2e99418fb7a4fd0ed81c885b01280ce5",
+    "url": "/static/media/corona.2e99418f.jpg"
+  },
+  {
+    "revision": "7be7f3040f149aef0bf787cbdcf659b0",
+    "url": "/static/media/dory.7be7f304.jpg"
+  },
+  {
+    "revision": "525bd3e6e54a5a8149713ee6124d4b30",
+    "url": "/static/media/elephant2.525bd3e6.jpg"
+  },
+  {
+    "revision": "3b455b9527ccc3704fb63f0f87c3d170",
+    "url": "/static/media/filledtransparentfrog.3b455b95.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d8d532387d3de3dc144b7bc972ec6af2",
+    "url": "/static/media/frogfront.d8d53238.jpg"
+  },
+  {
+    "revision": "0c6d2cf96ff188e32d50cb68b5bb7ada",
+    "url": "/static/media/ganesh1.0c6d2cf9.jpg"
+  },
+  {
+    "revision": "fdf44d7c1d967647836529a3869239fe",
+    "url": "/static/media/ganesha.fdf44d7c.jpg"
+  },
+  {
+    "revision": "351360e1f56542958374da6361cf5924",
+    "url": "/static/media/geodefish.351360e1.jpg"
+  },
+  {
+    "revision": "83fbb4e3252a15ae5477f0eece03c36f",
+    "url": "/static/media/geometric_wallhanger.83fbb4e3.jpg"
+  },
+  {
+    "revision": "0af41de1513d39bc640b2702afcb73aa",
+    "url": "/static/media/goldenpoisonfrog.0af41de1.png"
+  },
+  {
+    "revision": "fac7c1d5f88b89434aa63efc3278ccd4",
+    "url": "/static/media/jellyfish.fac7c1d5.jpg"
+  },
+  {
+    "revision": "3f66befe0395db106a98b7522f5ef07c",
+    "url": "/static/media/kryptonite.3f66befe.jpg"
+  },
+  {
+    "revision": "1baed8fa52db5418f153cde961acd1b9",
+    "url": "/static/media/mellon.1baed8fa.jpg"
+  },
+  {
+    "revision": "fb9ff73bc6e8e93059662cff1f738055",
+    "url": "/static/media/mike.fb9ff73b.jpg"
+  },
+  {
+    "revision": "b7f154a3fbed810848297521236e4b9b",
+    "url": "/static/media/narsil.b7f154a3.jpg"
+  },
+  {
+    "revision": "0630b415069902bb1021cbcd6ea25e21",
+    "url": "/static/media/origami_frogs.0630b415.jpg"
+  },
+  {
+    "revision": "5a838c57b11af9782be22c7ffe22cd27",
+    "url": "/static/media/peacock_wallhanger.5a838c57.jpg"
+  },
+  {
+    "revision": "4ac6a37bafc6c7961daeae47b0d92fce",
+    "url": "/static/media/rainbow_flower.4ac6a37b.jpg"
+  },
+  {
+    "revision": "ee696798cbbe0b6ff884ff727a0c0997",
+    "url": "/static/media/roses_wallhanger.ee696798.jpg"
+  },
+  {
+    "revision": "775d52294e22e544cd79087fe8a8c39d",
+    "url": "/static/media/sauron.775d5229.jpg"
+  },
+  {
+    "revision": "02fd6cd73db96566a8e8ab4c36be6bae",
+    "url": "/static/media/scylla_charybdis.02fd6cd7.jpg"
+  },
+  {
+    "revision": "d8c8d139696fabd34402d40bdbfd822b",
+    "url": "/static/media/selfportrait.d8c8d139.jpg"
+  },
+  {
+    "revision": "21d205224e21f3cf2b0cf1901b619c51",
+    "url": "/static/media/seminar.21d20522.jpg"
+  },
+  {
+    "revision": "bdf4654ea6799d778ee92bd5d9515787",
+    "url": "/static/media/skull.bdf4654e.jpg"
+  },
+  {
+    "revision": "fbb810001c6c739484dac1c7985378d4",
+    "url": "/static/media/sting.fbb81000.jpg"
+  },
+  {
+    "revision": "7bb4ebca91351151ccd46603dc1d7bc3",
+    "url": "/static/media/volcano.7bb4ebca.jpg"
+  }
+]);
