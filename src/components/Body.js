ba20dc878733cd378b9da7d645fc83b032f399bc
Krithika Ravishankar <krithika.x.ravishankar@gmail.com>
2021-06-22 01:11:57 -0500

    space

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,18 +4,41 @@ import { Switch, Link, Route, BrowserRouter as Router, IndexRoute } from 'react-
 import { FaMusic } from 'react-icons/fa'
 function Body(){
 	return (
-		<div className="box" style={{ fontSize: "1.2em"}}>
+		<div className="box" style={{ fontSize: "1.1em"}}>
 			<p style={{textAlign:"left"}}>
 				Hi! I'm Krithika!
 				<br /> <br/>
 				<FaMusic /> <i>These are a few of my favorite things ...</i> <FaMusic /> *
 				<br />
 
+
+				<ul>
+					<li><Link to="/writeup">Tech write-ups</Link></li>
+					<li>
+						<a href="https://drive.google.com/drive/folders/1vEIXVBMUBvCClC2yCGhrjkv4_BeWShKK?usp=sharing">
+							My uromastyx, Shakira
+						</a>
+					</li>
+					<li>
+						<a href="https://drive.google.com/drive/u/1/folders/1f8TGQ-XJU5tH5svkpFxOnAOk6lcPrhwb">
+							Reptile &amp; amphibian photos
+						</a>
+					</li>
+					<li>
+						<a href="https://github.com/krithravi/ESL-notes/blob/main/main.pdf">
+							Supplementary lecture notes for my ESL course
+						</a>
+					</li>
+
+				</ul>
+
+				{/*
 				<Link to="/writeup">
 					<button className="button introlinks">
 						Tech write-ups
 					</button>
 				</Link>
+
 				<a href="https://drive.google.com/drive/folders/1vEIXVBMUBvCClC2yCGhrjkv4_BeWShKK?usp=sharing">
 					<button className="button introlinks">
 						My uromastyx, Shakira
@@ -31,6 +54,7 @@ function Body(){
 						Supplementary lecture notes for my ESL course
 					</button>
 				</a>
+				*/}
 
 				<br /> <br />
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../style.css'
 import { Switch, Link, Route, BrowserRouter as Router, IndexRoute } from 'react-router-dom';
 import Contact from './Contact'
-var title="Krithika Ravishankar";
+var title="KRITHIKA RAVISHANKAR";
 
 function Header(){
 	return (
 		<div>
 					<h1>
 						<Link to="/" style={{textDecoration:"none"}}>
-							<div style={{color:"#fcdf20"}}>{title}</div>
+							<div style={{color:"#8fccf7"}}>{title}</div>
 						</Link>
 						<Contact />
 					</h1>
